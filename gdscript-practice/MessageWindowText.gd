8a9179cd1de8e11250fdfe69c88580a858ed6037
むずでょ <muzudho1@gmail.com>
2023-10-01 01:46:18 +0900

    Update MessageWindowText.gd

--- gdscript-practice/MessageWindowText.gd
+++ gdscript-practice/MessageWindowText.gd
@@ -18,7 +18,11 @@ var scenario_array = [
 	"""お父ん、なんで唐揚げを食べているんだぜ？
 	ダイエットはどうした？
 	野菜を TABERO だぜ！
-	"""
+	""",
+	"""元気になりたくて唐揚げを食べるんだぜ。
+	カロリー計算をしようと思ったときもあった。
+	限界まで食べていいと思ってしまうので止めた。
+	""",
 ]
 
 var text_storage = ""
@@ -43,3 +47,18 @@ func _process(delta):
 			self.text += text_storage.substr(0, 1)
 			text_storage = text_storage.substr(1, self.text_storage.length()-1)
 		count_of_typewriter -= 0.05
+
+func _unhandled_key_input(event):
+
+	print("_unhandled_key_input")
+
+	# 何かキーを押したとき
+	if event.is_pressed():
+		print("_unhandled_key_input is_pressed")
+		# TODO ブリンカーを消す
+		
+		# メッセージ送り
+		if self.text_storage == "" and 0 < self.scenario_array.size():
+			self.text = ""
+			self.text_storage = self.scenario_array.pop_front()
+		
