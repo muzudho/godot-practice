125054868621b42cdbedda5886299cea52fd94ec
むずでょ <muzudho1@gmail.com>
2023-10-01 01:32:58 +0900

    scenario array

--- gdscript-practice/MessageWindowText.gd
+++ gdscript-practice/MessageWindowText.gd
@@ -1,23 +1,45 @@
 extends Label
 
-var count = 0
+var count_of_typewriter = 0
 
-var text_storage = """お父ん、なんで唐揚げを食べているんだぜ？
-ダイエットはどうした？
-野菜を TABERO だぜ！
-"""
+var scenario_array = [
+	"""お父ん、知ってたら教えてくれだぜ。
+	エスフェン（SFEN）の　7g7f　って何だぜ？
+	""",
+	"""あー。７筋の７段目の駒を
+	６段目に突くことだぜ。
+	分かったら　もう寝ろ
+	""",
+	"""3c3d　って何だぜ？
+	""",
+	"""角換わりだろ。
+	もう寝ろ
+	""",
+	"""お父ん、なんで唐揚げを食べているんだぜ？
+	ダイエットはどうした？
+	野菜を TABERO だぜ！
+	"""
+]
+
+var text_storage = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# 最初のテキスト
 	self.text = ""
+	
+	if self.text_storage == "" and 0 < self.scenario_array.size():
+		self.text_storage = self.scenario_array.pop_front()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	count += delta
 	
-	if 0.05 <= count:
+	# タイプライター風出力
+	count_of_typewriter += delta
+	
+	if 0.05 <= count_of_typewriter:
 		if 0 < self.text_storage.length():
 			self.text += text_storage.substr(0, 1)
 			text_storage = text_storage.substr(1, self.text_storage.length()-1)
-		count -= 0.05
+		count_of_typewriter -= 0.05
