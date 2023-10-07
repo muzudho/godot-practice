2abaab6141793207fb822fd7360873243bc62330
むずでょ <muzudho1@gmail.com>
2023-10-07 22:44:52 +0900

    ブリンカー

--- gdscript-practice/scripts/MessageWindowBlinker.gd
+++ gdscript-practice/scripts/MessageWindowBlinker.gd
@@ -1,20 +1,21 @@
+#	メッセージエンド・ブリンカー（Message End Blinker；メッセージ末尾で点滅するもの）
 extends Label
 
-# メッセージ・ウィンドウの状態遷移図（親ノードがセットする）
+#	メッセージ・ウィンドウの状態遷移図（親ノードがセットする）
 var statemachine = null
 
-# 強制的に非表示にする
+#	強制的に非表示にする
 var is_hide_force = false
 
-# 点滅用
+#	カーソルが点滅するための時間カウント
 var is_first_displayed_immediately = false
-var count_of_blink = 0
+var blinker_seconds = 0.0
+var blinker_interval = 0.5
 
 
 func reset():
 	self.is_hide_force = false
 	self.visible = false
-	self.count_of_blink = 0
 	self.is_first_displayed_immediately = false
 
 
@@ -35,6 +36,18 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+
+	#	点滅
+	self.blinker_seconds += delta
+
+	if self.blinker_interval <= self.blinker_seconds:
+		if 0 < self.modulate.a:
+			self.modulate.a = 0.0
+		else:
+			self.modulate.a = 1.0
+			
+		self.blinker_seconds -= self.blinker_interval
+
 	
 	# （選択肢など、メッセージ末尾のブリンカーが要らないなら）
 	# 強制的に表示にするか？
@@ -45,19 +58,20 @@ func _process(delta):
 	
 	# 完全表示中	
 	if self.statemachine.is_completed():
-		
+
 		# 初回はすぐに表示
 		if not self.is_first_displayed_immediately:
 			self.visible = true
+			self.modulate.a = 1.0
+			self.blinker_seconds = 0.0
 			self.is_first_displayed_immediately = true
 
-		# 点滅
-		count_of_blink += delta
-		if 0.75 <= count_of_blink: 
-			visible = not visible
-			count_of_blink -= 0.75
-			
+		#	表示
+		elif not self.visible:
+			self.visible = true
+
 	# それ以外
 	else:
 		# 非表示
+		self.is_first_displayed_immediately = false
 		self.visible = false
