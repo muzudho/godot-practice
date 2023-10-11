93e2338a01310909b0d74794861ef1e389da6749
むずでょ <muzudho1@gmail.com>
2023-10-11 22:16:56 +0900

    show_scene

--- src-kifuwarabe-rpg/scripts/LocationCoordinator.gd
+++ src-kifuwarabe-rpg/scripts/LocationCoordinator.gd
@@ -8,23 +8,23 @@ func get_snapshot(department_node_name):
 
 
 #	シーンを表示する
-func show_scene(node_name):
-	print("［ロケーション・コーディネーター］　表示：[" + node_name + "]")
+func show_scene(department_node_name, location_node_name):
+	print("［ロケーション・コーディネーター］　表示：[" + location_node_name + "]")
 
 	#	既に表示中の画像を非表示にする（上に乗っかっていて、表示したい絵が見えないケースがある）
-	if self.get_snapshot("VisualNovelDepartment").location_node_name != null:
-		$"Images".get_node(self.get_snapshot("VisualNovelDepartment").location_node_name).hide()
+	if self.get_snapshot(department_node_name).location_node_name != null:
+		$"Images".get_node(self.get_snapshot(department_node_name).location_node_name).hide()
 
-	self.get_snapshot("VisualNovelDepartment").location_node_name = node_name
+	self.get_snapshot(department_node_name).location_node_name = location_node_name
 	self.visible = true
-	self.get_node(self.get_snapshot("VisualNovelDepartment").location_node_name).show()
+	self.get_node(self.get_snapshot(department_node_name).location_node_name).show()
 
 
 #	現在表示中のシーンがあれば、非表示にする
-func hide_current_scene():
-	if self.get_snapshot("VisualNovelDepartment").location_node_name == null:
+func hide_current_scene(department_node_name):
+	if self.get_snapshot(department_node_name).location_node_name == null:
 		return
 
-	print("［ロケーション・コーディネーター］　非表示：[" + str(self.get_snapshot("VisualNovelDepartment").location_node_name) + "]")
-	self.get_node(self.get_snapshot("VisualNovelDepartment").location_node_name).hide()
-	self.get_snapshot("VisualNovelDepartment").location_node_name = null
+	print("［ロケーション・コーディネーター］　非表示：[" + str(self.get_snapshot(department_node_name).location_node_name) + "]")
+	self.get_node(self.get_snapshot(department_node_name).location_node_name).hide()
+	self.get_snapshot(department_node_name).location_node_name = null
--- src-kifuwarabe-rpg/scripts/assistant_director/Scene.gd
+++ src-kifuwarabe-rpg/scripts/assistant_director/Scene.gd
@@ -6,10 +6,10 @@ extends Node
 #	それをする
 func do_it(line):
 
-	var node_name = line.substr(6).strip_edges()
-	print("［シーン］　名前：[" + node_name + "]")
+	var location_node_name = line.substr(6).strip_edges()
+	print("［シーン］　名前：[" + location_node_name + "]")
 
-	if node_name == "":
-		$"../../LocationCoordinator".hide_current_scene()
+	if location_node_name == "":
+		$"../../LocationCoordinator".hide_current_scene("VisualNovelDepartment")
 	else:
-		$"../../LocationCoordinator".show_scene(node_name)
+		$"../../LocationCoordinator".show_scene("VisualNovelDepartment", location_node_name)
