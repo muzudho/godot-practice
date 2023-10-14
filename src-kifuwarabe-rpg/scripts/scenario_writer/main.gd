d8a757c0cb476895c92bf84dead21860b8b1206d
むずでょ <muzudho1@gmail.com>
2023-10-15 02:36:54 +0900

    Departmentを一般化

--- src-kifuwarabe-rpg/scripts/scenario_writer/main.gd
+++ src-kifuwarabe-rpg/scripts/scenario_writer/main.gd
@@ -5,15 +5,15 @@ extends Node
 
 
 # 最初に実行する部門をここに書く
-var start_department = "VisualNovelDepartment"
+var start_department = "ビジュアルノベル部門"
 
 
 # どの部門で、そのキーを押されたら、どの部門へ移動するか？
 var key_pressed_mappings = {
-	"VisualNovelDepartment" : {
-		KEY_ESCAPE : "SystemMenuDepartment",
+	"ビジュアルノベル部門" : {
+		KEY_ESCAPE : "システムメニュー部門",
 	},
-	"SystemMenuDepartment" : {
-		KEY_ESCAPE : "VisualNovelDepartment",
+	"システムメニュー部門" : {
+		KEY_ESCAPE : "ビジュアルノベル部門",
 	},
 }
