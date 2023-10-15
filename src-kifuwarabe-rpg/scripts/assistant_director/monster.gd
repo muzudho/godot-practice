98bb9a6999d23c5b786b2d090df341b91b57ca6f
むずでょ <muzudho1@gmail.com>
2023-10-15 23:13:46 +0900

    monster command

--- src-kifuwarabe-rpg/scripts/assistant_director/monster.gd
+++ src-kifuwarabe-rpg/scripts/assistant_director/monster.gd
@@ -5,8 +5,21 @@ extends Node
 #	それをする
 func do_it(line):
 
-	var node_name = line.substr(8).strip_edges()
-	print("［背景］　ノード名：[" + node_name + "]")
+	var csv = line.substr(8).strip_edges()
+	print("［モンスター］　CSV：[" + csv + "]")
+	# 文字列の配列に分割
+	var string_packed_array = csv.split(",", true, 0)
 
-	# 背景設定
+	var node_name = string_packed_array[0]
+	var sub_command = null
+	
+	if 2 <= string_packed_array.size():
+		sub_command = string_packed_array[1].strip_edges()
+
+	if sub_command == "hide":
+		# モンスター画像非表示
+		$"../../MonsterTrainer".get_node(node_name).hide()
+		return
+
+	# モンスター画像表示
 	$"../../MonsterTrainer".get_node(node_name).show()
