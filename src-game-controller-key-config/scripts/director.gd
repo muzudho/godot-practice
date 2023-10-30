92f6c900345040b54091f030e84cfc11153ab7de
むずでょ <muzudho1@gmail.com>
2023-10-30 23:32:34 +0900

    WIP 状態遷移

--- src-game-controller-key-config/scripts/director.gd
+++ src-game-controller-key-config/scripts/director.gd
@@ -5,8 +5,9 @@ extends Node2D
 var re_axis_5 = RegEx.new()
 
 # 起動直後に　レバーが入った状態で始まることがあるから、１秒ぐらい無視するためのカウンター
-var counter_of_boot_ignore = 0.0
-var is_input_waiting = false
+var counter_of_wait = 0.0
+# WaitForPrompt, Prompt, WaitForInput, Input の４つ。 Wait を入れないと反応過敏になってしまう
+var turn_state = &"WaitForPrompt"
 var current_step = 1
 # 操作したボタン　（変数を増やしたくないのでレバーは＋１０００して入れる）
 var button_number = -1
@@ -29,38 +30,91 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
-	if counter_of_boot_ignore < 1.0:
-		counter_of_boot_ignore += delta
-		return
 	
-	if is_input_waiting:
+	if not (turn_state in [&"WaitForPrompt", &"Prompt", &"WaitForInput"]):
 		return
 	
 	var is_ok = false
 	
 	if self.current_step == 1:
-		$"GuiArtist/KeyConfig_CanvasLayer/決定ボタン".text = "（１）決定ボタン、メッセージ送りボタン　を押してください"
-		is_ok = true
+		if turn_state == &"WaitForPrompt":
+			if self.counter_of_wait < 1.0:
+				self.counter_of_wait += delta
+				return
+			turn_state = &"Prompt"
+		
+		elif turn_state == &"Prompt":
+			$"GuiArtist/KeyConfig_CanvasLayer/決定ボタン".text = "（１）決定ボタン、メッセージ送りボタン　を押してください"
+			turn_state = &"WaitForInput"
+
+		elif turn_state == &"WaitForInput":
+			if self.counter_of_wait < 1.0:
+				self.counter_of_wait += delta
+				return
+			turn_state = &"Prompt"
+			is_ok = true
 		
 	elif self.current_step == 2:
-		$"GuiArtist/KeyConfig_CanvasLayer/決定ボタン".text = "（１）決定ボタン、メッセージ送りボタン：　" + self.button_presentation_name
-		$"GuiArtist/KeyConfig_CanvasLayer/キャンセルボタン".text = "（２）キャンセルボタン、メニューボタン　を押してください"
-		is_ok = true
+		if turn_state == &"WaitForPrompt":
+			if self.counter_of_wait < 1.0:
+				self.counter_of_wait += delta
+				return
+			turn_state = &"Prompt"
+		
+		elif turn_state == &"Prompt":
+			$"GuiArtist/KeyConfig_CanvasLayer/決定ボタン".text = "（１）決定ボタン、メッセージ送りボタン：　" + self.button_presentation_name
+			$"GuiArtist/KeyConfig_CanvasLayer/キャンセルボタン".text = "（２）キャンセルボタン、メニューボタン　を押してください"
+			turn_state = &"WaitForInput"
+
+		elif turn_state == &"WaitForInput":
+			if self.counter_of_wait < 1.0:
+				self.counter_of_wait += delta
+				return
+			turn_state = &"Prompt"
+			is_ok = true
 		
 	elif self.current_step == 3:
-		$"GuiArtist/KeyConfig_CanvasLayer/キャンセルボタン".text = "（２）キャンセルボタン、メニューボタン：　" + self.button_presentation_name
-		$"GuiArtist/KeyConfig_CanvasLayer/メッセージ早送りボタン".text = "（３）メッセージ早送りボタン　を押してください"
-		is_ok = true
+		if turn_state == &"WaitForPrompt":
+			if self.counter_of_wait < 1.0:
+				self.counter_of_wait += delta
+				return
+			turn_state = &"Prompt"
+		
+		elif turn_state == &"Prompt":
+			$"GuiArtist/KeyConfig_CanvasLayer/キャンセルボタン".text = "（２）キャンセルボタン、メニューボタン：　" + self.button_presentation_name
+			$"GuiArtist/KeyConfig_CanvasLayer/メッセージ早送りボタン".text = "（３）メッセージ早送りボタン　を押してください"
+			turn_state = &"WaitForInput"
+
+		elif turn_state == &"WaitForInput":
+			if self.counter_of_wait < 1.0:
+				self.counter_of_wait += delta
+				return
+			turn_state = &"Prompt"
+			is_ok = true
 
 	elif self.current_step == 4:
-		$"GuiArtist/KeyConfig_CanvasLayer/メッセージ早送りボタン".text = "（３）メッセージ早送りボタン：　" + self.button_presentation_name
-		is_ok = true
+		if turn_state == &"WaitForPrompt":
+			if self.counter_of_wait < 1.0:
+				self.counter_of_wait += delta
+				return
+			turn_state = &"Prompt"
+		
+		elif turn_state == &"Prompt":
+			$"GuiArtist/KeyConfig_CanvasLayer/メッセージ早送りボタン".text = "（３）メッセージ早送りボタン：　" + self.button_presentation_name
+			turn_state = &"WaitForInput"
+
+		elif turn_state == &"WaitForInput":
+			if self.counter_of_wait < 1.0:
+				self.counter_of_wait += delta
+				return
+			turn_state = &"Prompt"
+			is_ok = true
 	
 	if is_ok:
+		self.counter_of_wait = 0.0
 		self.current_step += 1
 		self.button_number = -1
 		self.button_presentation_name = &""
-		self.is_input_waiting = true
 
 
 func _unhandled_input(event):
@@ -68,11 +122,8 @@ func _unhandled_input(event):
 	# 起動直後に、押してもないレバーが　押したことになっていることがある
 	var event_as_text = event.as_text()
 	print("入力：　" + event_as_text)
-
-	if counter_of_boot_ignore < 1.0:
-		return
 	
-	if not is_input_waiting:
+	if turn_state != &"Prompt":
 		return
 
 	var is_ok = false
@@ -94,4 +145,4 @@ func _unhandled_input(event):
 
 	if is_ok:
 		print(acception)
-		self.is_input_waiting = false
+		turn_state = &"WaitForPrompt"
