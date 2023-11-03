0d4d88272fc5e5cb412c9c0327b764b2f3f5cd2d
むずでょ <muzudho1@gmail.com>
2023-11-03 11:42:34 +0900

    ノード分離

--- src-game-controller-key-config/key_config_artist.gd
+++ src-game-controller-key-config/key_config_artist.gd
@@ -25,6 +25,9 @@ var current_step = 0
 # WaitForPrompt, Prompt, WaitForInput, Input, InputOk の５つ。 Wait を入れないと反応過敏になってしまう
 var turn_state = &"WaitForPrompt"
 
+# `.entry()` を呼び出すと真にする。キー・コンフィグが完了するとまた偽にセットする
+var is_enabled = false
+
 
 # テロップ・コーディネーター取得
 func get_telop_coordinator():
@@ -59,6 +62,7 @@ func entry():
 	self.get_telop_coordinator().get_node("TextBlock").text = """\
 	＊　＊　＊
 	"""
+	self.is_enabled = true
 
 
 func set_key_ok():
@@ -72,6 +76,10 @@ func set_key_denied():
 
 
 func on_process(delta):
+
+	if not self.is_enabled:
+		return
+	
 	if not (self.turn_state in [&"WaitForPrompt", &"Prompt", &"WaitForInput", &"InputOk"]):
 		return
 	
@@ -200,6 +208,7 @@ func on_process(delta):
 				self.counter_of_wait += delta
 				return
 			self.turn_state = &"Input"
+			self.is_enabled = false
 			is_ok = true
 	
 	else:
@@ -209,3 +218,42 @@ func on_process(delta):
 		self.counter_of_wait = 0.0
 		self.button_number = -1
 		self.button_presentation_name = &""
+
+
+func on_unhandled_input(event):
+
+	if not self.is_enabled:
+		return
+
+	# 起動直後に、押してもないレバーが　押したことになっていることがある
+	var event_as_text = event.as_text()
+	print("入力：　" + event_as_text)
+	
+	if self.turn_state != &"Input":
+		return
+
+	var is_ok = false
+	var acception = "受付：　"
+
+	# 📖　[enum JoyButton:](https://docs.godotengine.org/en/stable/classes/class_%40globalscope.html#enum-globalscope-joybutton)
+	# レバーは -1 ～ 10、 ボタンは -1 ～ 128 まであるそうだ
+	if not is_ok:
+		var matched = self.re_button.search(event_as_text)
+		if matched:
+			self.button_number = int(matched.get_string(1))
+			self.button_presentation_name = "ボタン" + str(self.button_number)
+			is_ok = true
+
+	if not is_ok:
+		var matched = self.re_lever.search(event_as_text)
+		if matched:
+			var number = int(matched.get_string(1))
+			self.button_presentation_name = "レバー" + str(number)
+			self.button_number = number + 1000
+			is_ok = true
+
+	if is_ok:
+		print(acception)
+		self.turn_state = &"InputOk"
+		self.get_musician().get_node("SE/🔔キーコンフィグ決定音").play()
+		print("入力完了")
--- src-game-controller-key-config/scripts/director.gd
+++ src-game-controller-key-config/scripts/director.gd
@@ -4,45 +4,16 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# キー・コンフィグ画面を始める
 	$"KeyConfigArtist".entry()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# キー・コンフィグで必要
 	$"KeyConfigArtist".on_process(delta)
 
 
 func _unhandled_input(event):
-
-	# 起動直後に、押してもないレバーが　押したことになっていることがある
-	var event_as_text = event.as_text()
-	print("入力：　" + event_as_text)
-	
-	if $"KeyConfigArtist".turn_state != &"Input":
-		return
-
-	var is_ok = false
-	var acception = "受付：　"
-
-	# 📖　[enum JoyButton:](https://docs.godotengine.org/en/stable/classes/class_%40globalscope.html#enum-globalscope-joybutton)
-	# レバーは -1 ～ 10、 ボタンは -1 ～ 128 まであるそうだ
-	if not is_ok:
-		var matched = $"KeyConfigArtist".re_button.search(event_as_text)
-		if matched:
-			$"KeyConfigArtist".button_number = int(matched.get_string(1))
-			$"KeyConfigArtist".button_presentation_name = "ボタン" + str($"KeyConfigArtist".button_number)
-			is_ok = true
-
-	if not is_ok:
-		var matched = $"KeyConfigArtist".re_lever.search(event_as_text)
-		if matched:
-			var number = int(matched.get_string(1))
-			$"KeyConfigArtist".button_presentation_name = "レバー" + str(number)
-			$"KeyConfigArtist".button_number = number + 1000
-			is_ok = true
-
-	if is_ok:
-		print(acception)
-		$"KeyConfigArtist".turn_state = &"InputOk"
-		$"Musician/SE/🔔キーコンフィグ決定音".play()
-		print("入力完了")
+	# キー・コンフィグで必要
+	$"KeyConfigArtist".on_unhandled_input(event)
