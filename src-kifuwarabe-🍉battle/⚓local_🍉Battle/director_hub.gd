9355177b7ed2dd75e927e061cdb6ef56a6c7c53c
むずでょ <muzudho1@gmail.com>
2024-01-17 21:27:19 +0900

    hub

--- src-kifuwarabe-🍉battle/⚓local_🍉Battle/director_hub.gd
+++ src-kifuwarabe-🍉battle/⚓local_🍉Battle/director_hub.gd
@@ -12,51 +12,61 @@ func owner_node():
 	return self.get_node("../../🌏Director")
 
 
+# グリッド取得
+func grid():
+	return self.owner_node().get_node("Grid")
+
+
 # イラストレーター取得
 func illustrator():
-	return self.get_node("🌏Illustrator")
+	return self.owner_node().get_node("🌏Illustrator")
 
 
 # メッセージ・ウィンドウズ取得
 func gui_programmer_message_windows():
-	return self.get_node("🌏Programmer_MessageWindow")
+	return self.owner_node().get_node("🌏Programmer_MessageWindow")
 
 
 # モンスターの全身像
 func monster_whole_body():
-	return self.get_node("MonsterTrainer/WholeBody")
+	return self.owner_node().get_node("MonsterTrainer/WholeBody")
 
 
 # モンスター・フェースズ
 func monster_faces():
-	return self.get_node("MonsterTrainer/Faces")
+	return self.owner_node().get_node("MonsterTrainer/Faces")
 
 
 # BGM取得
 func musician_bg_musics():
-	return self.get_node("🌏Musician/🌏BgMusics")
+	return self.owner_node().get_node("🌏Musician/🌏BgMusics")
 
 
 # SE取得
 func musician_sound_fx():
-	return self.get_node("🌏Musician/🌏SoundFX")
+	return self.owner_node().get_node("🌏Musician/🌏SoundFX")
 
 
 # プログラマー取得
 func programmer():
-	return self.get_node("🌏Programmer")
+	return self.owner_node().get_node("🌏Programmer")
+
+
+# プログラマー・ハブ取得
+func programmer_hub():
+	return self.owner_node().get_node("🌏Programmer/🛩️Hub")
 
 
 # シナリオライター取得
 func scenario_writer():
-	return self.get_node("🌏ScenarioWriter")
+	return self.owner_node().get_node("🌏ScenarioWriter")
 
 
 # シナリオライター・ハブ取得
 func scenario_writers_hub():
-	return self.get_node("🌏ScenarioWriter/🛩️Hub_🍉VisualNovel")
+	return self.owner_node().get_node("🌏ScenarioWriter/🛩️Hub_🍉VisualNovel")
 
 
 # テロップ・コーディネーター取得
 func telop_coordinator():
-	return self.get_node("🌏TelopCoordinator")
+	return self.owner_node().get_node("🌏TelopCoordinator")
