e8c576d343dccf548851679218c2807aded3e2dd
むずでょ <muzudho1@gmail.com>
2024-02-23 15:55:42 +0900

    Battle / KeyParse

---
+++ src-kifuwarabe-🍉battle/⚓local_🍉Battle/programmer_input_extension.gd
@@ -0,0 +1,55 @@
+# インプット　＞　エクステンション（Extension；拡張）
+extends Node
+
+
+# ーーーーーーーー
+# ノード・パス関連
+# ーーーーーーーー
+
+# 猿取得
+func monkey():
+	return $"../../🐵Monkey"
+
+# オーナー取得
+func owner_node():
+	return $"../../🕹️Input"
+
+
+# ーーーーーーーー
+# イベント・ハンドラー
+# ーーーーーーーー
+
+func on_process(_delta):
+	pass
+
+
+func on_handled_input(event):
+	# ［まだ準備ができていません］
+	if self.monkey().owner_node().current_state == &"NotReadyYet":
+		pass
+
+	# ［キー・コンフィグで］状態
+	elif self.monkey().owner_node().current_state == &"InKeyConfig":
+		self.monkey().key_config_node().on_unhandled_input(event)
+
+	# ［シナリオで］状態
+	elif self.monkey().owner_node().current_state == &"InScenario":
+		self.monkey().scenario_player().input_node().on_unhandled_input(event)
+
+
+# テキストボックスなどにフォーカスが無いときのキー入力を拾う
+#
+# 子要素から親要素の順で呼び出されるようだ。
+# このプログラムでは　ルート　だけで　キー入力を拾うことにする
+func _unhandled_key_input(event):
+	# ［まだ準備ができていません］
+	if self.monkey().owner_node().current_state == &"NotReadyYet":
+		pass
+
+	# ［キー・コンフィグで］は、何もするな
+	elif self.monkey().owner_node().current_state == &"InKeyConfig":
+		pass
+
+	# ［シナリオで］状態
+	elif self.monkey().owner_node().current_state == &"InScenario":
+		self.monkey().scenario_player().input_node().on_unhandled_key_input(event)
