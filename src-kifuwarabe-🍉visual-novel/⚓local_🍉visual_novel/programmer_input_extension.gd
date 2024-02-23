3ed3456ecd8a4b3a21d56b73be83a6966c699525
むずでょ <muzudho1@gmail.com>
2024-02-23 16:39:57 +0900

    WIP VisualNovel / Input 仕様変更中

--- src-kifuwarabe-🍉visual-novel/⚓local_🍉visual_novel/programmer_input_extension.gd
+++ src-kifuwarabe-🍉visual-novel/⚓local_🍉visual_novel/programmer_input_extension.gd
@@ -65,10 +65,6 @@ func on_handled_input(event):
 		# キーコンフィグで必要です
 		self.monkey().key_config_node().on_unhandled_input(event)
 
-	# ［シナリオで］状態
-	elif self.monkey().owner_node().current_state == &"InScenario":
-		self.monkey().scenario_player().input_node().on_unhandled_input(event)
-
 
 # テキストボックスなどにフォーカスが無いときのキー入力を拾う
 #
@@ -85,4 +81,45 @@ func _unhandled_key_input(event):
 
 	# ［シナリオで］状態
 	elif self.monkey().owner_node().current_state == &"InScenario":
-		self.monkey().scenario_player().input_node().on_unhandled_key_input(event)
+		var vk_operation = null
+
+		# 何かキーを押したとき
+		if event.is_pressed():
+			print("［監督］　キー入力　押下")
+			vk_operation = &"VKO_Pressed"
+		
+		# 何かキーを離したとき
+		elif event.is_released():
+			print("［監督］　キー入力　リリース")
+			vk_operation = &"VKO_Released"
+		
+		# それ以外には対応してない
+		else:
+			print("［監督］　キー入力　▲！想定外")
+			return
+
+		# 以下、仮想キー
+
+		# このゲーム独自の仮想キーに変換
+		var virtual_key_name = null
+		
+		# エンターキー押下
+		if event.keycode == KEY_ENTER:
+			virtual_key_name = &"VK_Ok"
+
+		# エスケープキー押下
+		elif event.keycode == KEY_ESCAPE:
+			virtual_key_name = &"VK_Cancel"
+
+		# ［Ｒ］キー押下（後でスーパーファミコンの R キーにしようと思っていたアルファベット）
+		elif event.keycode == KEY_R:
+			virtual_key_name = &"VK_FastForward"
+		
+		# それ以外のキーは無視する（十字キーや Ctrl キーの判定を取り除くのが難しい）
+		else:
+			return
+
+		var lever_value = 0.0
+
+		# 仮想キーを押下したという建付け
+		self.monkey().of_staff().programmer().scenario_player().input_node().on_virtual_key_input(virtual_key_name, lever_value, vk_operation)
